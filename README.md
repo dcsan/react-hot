df0d419b54c2f72f27f07334f4362198c98c6440
dcsan <dc@pikkle.com>
2015-12-16 14:06:31 -0800

    tried to add markdown renderer but seems broken https://github.com/Jpunt/meteor-react-markdown/issues/2

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # react-hot
+
+using material-ui with React and meteor
+
+demo: http://react-hot.meteor.com
+source:
+https://github.com/dcsan/react-hot
+
+using:
+- handsontable  http://docs.handsontable.com/
+
+- http://www.material-ui.com/#/
+
+examples and source code eg:  
+
+- [docs for LeftNav](https://github.com/callemall/material-ui/tree/master/docs/src/app/components/LeftNav)
+
+- source implementation of [MenuItem](https://github.com/callemall/material-ui/blob/master/src/menus/menu-item.jsx)
+
+----
--- app/client/top/Top.jsx
+++ app/client/top/Top.jsx
@@ -1,17 +1,41 @@
+/* global Markdown */
+
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 TopComponent = React.createClass({
+
+  content() {
+    return (
+<Markdown>
+# using material-ui with React and meteor
+
+demo: http://react-hot.meteor.com
+source: https://github.com/dcsan/react-hot
+
+using:
+- handsontable  http://docs.handsontable.com/
+- material-ui  http://www.material-ui.com/#/
+
+review examples and source code eg:
+- [docs for LeftNav](https://github.com/callemall/material-ui/tree/master/docs/src/app/components/LeftNav)
+- source implementation of [MenuItem](https://github.com/callemall/material-ui/blob/master/src/menus/menu-item.jsx)
+</Markdown>
+    );
+  },
 
   render: function() {
 
     return (
       <div className='main'>
-        <h1>React + H.O.T.</h1>
-          <ul>
-            <li>
-              <a href='/table'>Users</a>
-            </li>
-          </ul>
+        <h2>React + HandsOnTable</h2>
+        <ul>
+          <li>
+            <a href='/table'>handsontable</a>
+          </li>
+        </ul>
+        <div>
+{this.content()}
+        </div>
       </div>
     );
   }
